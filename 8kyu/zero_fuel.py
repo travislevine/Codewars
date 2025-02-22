ff7386260963a760fb97bb77adcfcc203e7c78ac
Travis <sneakyplutosaturn@gmail.com>
2025-02-23 08:54:18 +1000

    8kyu complete string_repeat.py

--- 8kyu/zero_fuel.py
+++ 8kyu/zero_fuel.py
@@ -1,2 +1,3 @@
 def zeroFuel(distance_to_pump, mpg, fuel_left):
     return distance_to_pump <= mpg * fuel_left
+#You have passed all of the tests! :)
---
+++ string_repeat.py
@@ -0,0 +1,6 @@
+def repeat_str(repeat, string):
+    return string * repeat
+
+print(repeat_str(3, "I"))  # Output: "III"
+
+#You have passed all of the tests! :)
