d243b6868cb636dadb2ad1696c4afbc9589f72e8
Travis <sneakyplutosaturn@gmail.com>
2025-04-18 09:29:00 +1000

    CodeWars 8kyu: Add solution for  traffic_light.py

---
+++ 8kyu/Python/traffic_light.py
@@ -0,0 +1,11 @@
+def update_light(current):
+    # Your code here.
+    if current == "green":
+        return "yellow"
+    elif current == "yellow":
+        return "red"
+    elif current == "red":
+        return "green"
+
+#https://www.codewars.com/kata/58649884a1659ed6cb000072/train/python
+#You have passed all of the tests! :)
