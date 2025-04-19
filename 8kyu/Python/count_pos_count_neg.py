964eb81bfa451b6aa85d3ab83ac377c316241dd9
Travis <sneakyplutosaturn@gmail.com>
2025-04-19 11:02:12 +1000

    CodeWars 8kyu: Add solution for count_pos / sum of negatives

--- 8kyu/Python/count_pos_count_neg.py
+++ 8kyu/Python/count_pos_count_neg.py
@@ -1,2 +1,14 @@
 def count_positives_sum_negatives(arr):
-    return
+    if arr == None or arr == []:
+        return []
+    positive_count = 0
+    negative_sum = 0
+    for num in arr:
+        if num > 0:
+            positive_count += 1
+        elif num < 0:
+            negative_sum += num
+    new_list = [positive_count, negative_sum]
+    return new_list
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/python
