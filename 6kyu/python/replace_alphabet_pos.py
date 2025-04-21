f131b35009e79a7296d4ff3779f056ed3493e433
Travis <sneakyplutosaturn@gmail.com>
2025-04-21 17:48:44 +1000

    CodeWars 6kyu: Add solution for replace alphabet position

---
+++ 6kyu/python/replace_alphabet_pos.py
@@ -0,0 +1,15 @@
+
+def alphabet_position(text):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    postions = ""
+    for char in text:
+        if char.isalpha():
+            postions += str(alphabet.index(char.lower()) + 1)  + " "
+        else:
+            continue
+    return postions.strip()
+
+    
+print(alphabet_position("The sunset sets at twelve o' clock."))
+#https://www.codewars.com/kata/546f922b54af40e1e90001da/train/python
+#You have passed all of the tests! :)
