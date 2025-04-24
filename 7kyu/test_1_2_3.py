7b65f954e26809f765132cdf0bfd96c2528d2ea3
Travis <sneakyplutosaturn@gmail.com>
2025-04-25 06:33:24 +1000

    CodeWars 7kyu: Comple solution testing 1 2 3

--- 7kyu/test_1_2_3.py
+++ 7kyu/test_1_2_3.py
@@ -1,3 +1,14 @@
 def number(lines):
     #your code here
-    pass
+    # Stores the final result of strings with numbers
+    modified = []
+    count = 0
+    for line in lines:
+        count += 1
+        current_str = line
+        apnd_str = f"{count}: {current_str}"
+        modified.append(apnd_str)
+    return modified
+
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/python
