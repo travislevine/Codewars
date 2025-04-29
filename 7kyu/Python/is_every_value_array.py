a2a3412874eaa6dd5ced107a46b2e2e964864d31
Travis <sneakyplutosaturn@gmail.com>
2025-04-30 09:24:37 +1000

    CodeWars 7kyu: Added solution for is array item an array

--- 7kyu/Python/is_every_value_array.py
+++ 7kyu/Python/is_every_value_array.py
@@ -1,4 +1,9 @@
 def arr_check(arr):
+    for item in arr:
+        if type(item) is not list:
+            return False
+        else:
+            continue
     return True
-
+#You have passed all of the tests! :)
 #https://www.codewars.com/kata/582c81d982a0a65424000201/train/python
