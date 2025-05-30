cd0e3db1d7644c2967c3bc72eb4b1c21204e4af1
Travis <sneakyplutosaturn@gmail.com>
2025-05-31 08:08:04 +1000

    CodeWars 6kyu: Added alternative solution for are_they_same.py

--- 6kyu/python/are_they_same.py
+++ 6kyu/python/are_they_same.py
@@ -4,3 +4,10 @@ def comp(array1, array2):
     elif len(array1) != len(array2):
         return False
     return sorted([x ** 2 for x in array1]) == sorted(array2)
+
+#Alternative solution
+def comp(array1, array2):
+    try:
+        return sorted([i ** 2 for i in array1]) == sorted(array2) # It seems each element in array1 is squared and sorted
+    except: #This except accounts for NULL arrays and if the array's lengths don't match
+        return False 
