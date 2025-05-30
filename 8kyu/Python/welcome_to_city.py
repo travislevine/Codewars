5a86daae8f74342f3e11900a9764133e4c0f5cbf
Travis <sneakyplutosaturn@gmail.com>
2025-05-31 08:27:57 +1000

    Accidentally did an amend

--- 8kyu/Python/welcome_to_city.py
+++ 8kyu/Python/welcome_to_city.py
@@ -1,2 +1,6 @@
 def say_hello(name, city, state):
-    #your code here
+    result_string = ' '.join(name)
+    return f"Hello, {result_string}! Welcome to {city}, {state}!"
+    
+print(say_hello(['John', 'Smith'], "Phoenix", "Arizona"))
+#You have passed all of the tests! :)
