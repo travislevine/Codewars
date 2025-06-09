4e5e327e12eb78ab30e07dc3e1c6872a8478ea95
Travis <sneakyplutosaturn@gmail.com>
2025-06-09 13:11:10 +1000

    CodeWars 8kyu: Added solution for bool_to_word.py

---
+++ 8kyu/Python/bool_to_word.py
@@ -0,0 +1,5 @@
+def bool_to_word(boolean):
+    res = "Yes" if boolean == True else "No"
+    return res
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/53369039d7ab3ac506000467/train/python
