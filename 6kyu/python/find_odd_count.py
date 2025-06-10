c4c0933b08ad7595872181e2423658d4e44ceecb
Travis <sneakyplutosaturn@gmail.com>
2025-06-10 11:01:20 +1000

    CodeWars 6kyu: Added solution for find_odd_count.py

--- 6kyu/python/find_odd_count.py
+++ 6kyu/python/find_odd_count.py
@@ -1,2 +1,14 @@
 def find_it(seq):
-    return None
+    # Create an empty dictionary to store the counts
+    dic = {}
+    for num in seq:
+        if num in dic:
+            dic[num] += 1
+        else:
+            dic[num] = 1
+    for count in dic:
+        if dic[count] % 2 != 0:
+            return count
+print(find_it([1, 2, 2, 3, 3]))
+#https://www.codewars.com/kata/54da5a58ea159efa38000836/train/python
+#You have passed all of the tests! :)
