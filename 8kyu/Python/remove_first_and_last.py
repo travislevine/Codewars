cc66dfc1ef56d6c0b2ca170f6e098af934cfb27a
Travis <sneakyplutosaturn@gmail.com>
2025-06-11 08:42:28 +1000

    CodeWars 8kyu: Added solution for remove_first_and_last.py

---
+++ 8kyu/Python/remove_first_and_last.py
@@ -0,0 +1,6 @@
+def remove_char(s):
+    #your code here
+    return s[1:-1]
+print(remove_char("hello"))
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/python
