924fbc7462284b65b5a037d4c37d0ececfcefcf2
Travis <sneakyplutosaturn@gmail.com>
2025-06-13 13:00:17 +1000

    CodeWars 8kyu:  Added solution for is_even.py

---
+++ 8kyu/Python/is_it_even.py
@@ -0,0 +1,8 @@
+def is_even(n): 
+    # your code here
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/python
