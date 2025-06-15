474fa44ebd8b25620d15407cfec2376af8f83a2d
Travis <sneakyplutosaturn@gmail.com>
2025-06-15 20:44:11 +1000

    CodeWars 7kyu: Complete solution for sum_two_smallest_numbers.py

---
+++ 7kyu/sum_of_two_lowest_ints.py
@@ -0,0 +1,8 @@
+# def sum_two_smallest_numbers(numbers):
+#     #your code here
+#     for index, item in enumerate(numbers, start=1):
+#         print(index, item)
+
+numbers = [5, 8, 12, 18, 22]
+for index, item in enumerate(numbers, start=1):
+        print(index, item)
