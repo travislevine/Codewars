6a5428d795e53197dc326736d64a3fc959abb337
Travis <sneakyplutosaturn@gmail.com>
2025-06-25 02:39:01 +1000

    CodeWars 7kyu: Complete solution for credit_card_mask.py

--- 7kyu/Python/credit_card_mask.py
+++ 7kyu/Python/credit_card_mask.py
@@ -1,4 +1,13 @@
-# return masked string
+# Revise this solution and understand the string slicing bit
 def maskify(cc):
-
-    pass
+    str_length = len(cc)
+    if str_length <= 4:
+        return cc
+    else:
+        unmasked_part = cc[-4:]
+        number_of_hashes = len(cc) - 4 #Revise this solution and understand the string slicing bit
+        masked_part = '#' * number_of_hashes
+        result = masked_part + unmasked_part
+        return result
+    #You have passed all of the tests! :)
+    #https://www.codewars.com/kata/5412509bd436bd33920011bc/train/python
