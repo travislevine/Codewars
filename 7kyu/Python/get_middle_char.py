befacd0a791e3068a97994dc65e29dd162f05805
Travis <sneakyplutosaturn@gmail.com>
2025-06-25 02:15:39 +1000

    CodeWars 7kyu: Complete solution for get_middle_char.py

--- 7kyu/Python/get_middle_char.py
+++ 7kyu/Python/get_middle_char.py
@@ -1,2 +1,10 @@
 def get_middle(s):
-    pass
+    str_length = len(s)
+    middle = str_length // 2
+
+    if str_length % 2  == 0:
+        return s[middle - 1: middle + 1] # Revise this line
+    else: 
+        return s[middle]
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/56747fd5cb988479af000028/train/python
