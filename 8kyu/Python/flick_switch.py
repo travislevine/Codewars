2f90b6068f73a5c0267ef51242d549d06d9aa070
Travis <sneakyplutosaturn@gmail.com>
2025-06-25 02:08:22 +1000

    CodeWars 8kyu: Added solution flick_switch.py

--- 8kyu/Python/flick_switch.py
+++ 8kyu/Python/flick_switch.py
@@ -1,2 +1,12 @@
 def flick_switch(lst):
-    pass
+    current_value = True
+    result_list = []
+    for element in lst:
+        if element == "flick":
+            current_value = not current_value 
+            result_list.append(current_value)
+        else:
+            result_list.append(current_value)
+    return result_list
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/64fbfe2618692c2018ebbddb/train/python
