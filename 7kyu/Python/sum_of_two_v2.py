5e8c850e938c1d61dfa66844cdb2ce9d66cba3f7
Travis <sneakyplutosaturn@gmail.com>
2025-06-26 18:41:29 +1000

    CodeWars 7kyu: Added solution sum_two_smallest_nums.py

--- 7kyu/Python/sum_of_two_v2.py
+++ 7kyu/Python/sum_of_two_v2.py
@@ -1,3 +1,12 @@
 def sum_two_smallest_numbers(numbers):
-    #your code here
-    pass
+    #Sorting the numbers list in ascending order into a new list
+    ascending_list = sorted(numbers)
+
+    #Adding thw two smallest numbers together
+    add_two_lowest = ascending_list[0] + ascending_list[1]
+    ascending_list.append(add_two_lowest)
+    
+    return add_two_lowest
+print(sum_two_smallest_numbers([10, 343445353, 3453445, 3453545353453])) 
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/python
