cdaa8ed94b5986e17a5578e7235d06c03c224fc6
Travis <sneakyplutosaturn@gmail.com>
2025-06-26 10:16:53 +1000

    CodeWars 7kyu: Added solution for longest_str.py

--- 7kyu/Python/longest_str.py
+++ 7kyu/Python/longest_str.py
@@ -1,3 +1,8 @@
 def longest(a1, a2):
     # your code
-    pass
+    a3 = a1 + a2
+    unique_alphabet_set = sorted(set(a3))
+    unique_str = "".join(unique_alphabet_set)
+    return unique_str
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/python
