78adcd47bcf7374961ad8da4c9cb712acd4f13f9
Travis <sneakyplutosaturn@gmail.com>
2025-06-27 14:17:43 +1000

    CodeWars 8kyu: Added solution for array_plus_array.py and twice_as_old.py

---
+++ 8kyu/Python/array_plus_array.py
@@ -0,0 +1,14 @@
+def array_plus_array(arr1,arr2):
+    sum_of_two = 0
+    for number in arr1:
+        # Add all the elements in arr1 to 
+        #First loop through each element, adding each one to the sum_of_two variable
+        sum_of_two += number
+
+    for number in arr2:
+    # Add all the elements in arr2 to sum_of_two
+    #First loop through each element, adding each one to the sum_of_two variable
+        sum_of_two += number
+    return sum_of_two
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/python
---
+++ 8kyu/Python/twice_as_old.py
@@ -0,0 +1,7 @@
+def twice_as_old(dad_years_old, son_years_old):
+    doubled_son_age = son_years_old * 2
+    age_difference  = dad_years_old - doubled_son_age
+    years_ago_or_hence = abs(age_difference)
+    return years_ago_or_hence
+#https://www.codewars.com/kata/5b853229cfde412a470000d0/train/python
+#You have passed all of the tests! :)
