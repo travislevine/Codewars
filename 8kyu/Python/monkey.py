2bf2254f361e8ac20abdb77fdff4e770ea674a35
Travis <sneakyplutosaturn@gmail.com>
2025-06-28 11:28:22 +1000

    CodeWars 8kyu: Added solution for monkey_count.py

---
+++ 8kyu/Python/monkey.py
@@ -0,0 +1,7 @@
+def monkey_count(n):
+    monkey_count_list = []
+    for num in range(1, n + 1):
+        monkey_count_list.append(num)
+    return monkey_count_list
+#https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/train/python
+#You have passed all of the tests! :)
