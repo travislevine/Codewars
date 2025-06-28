c3d0165e2ec782af008089c9d5c726de07230d09
Travis <sneakyplutosaturn@gmail.com>
2025-06-28 11:15:27 +1000

    CodeWars 8kyu: Added solution for sum_tri_odd.py

---
+++ 7kyu/Python/sum_tri_odd.py
@@ -0,0 +1,12 @@
+def row_sum_odd_numbers(n):
+    #your code here
+    # Step 1: 'n' is the input from the function argument.
+
+    # Step 2: Calculate n to the power of 3.
+    # We'll use the standard exponent operator.
+    the_sum = n ** 3
+
+    # Step 3: Return the final result.
+    return the_sum
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/python
