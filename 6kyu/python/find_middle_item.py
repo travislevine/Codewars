a8015836623cac292a9f169397204ed035ed5ed6
Travis <sneakyplutosaturn@gmail.com>
2025-07-09 09:51:14 +1000

    CodeWars *7kyu* not *6kyu*: Added solution for find_middle_item.py

--- 6kyu/python/find_middle_item.py
+++ 6kyu/python/find_middle_item.py
@@ -1,4 +1,14 @@
 def gimme(input_array):
     # Implement this function
-    ...
     
+    # 1: Sort the input_array (can be manual or python built in)
+    sorted_inp_arr = sorted(input_array)
+
+    #2: Find the middle value 
+    middle_value = sorted_inp_arr[1]
+
+    #3: Find original value index in input_array
+    original_value = input_array.index(middle_value)
+
+#https://www.codewars.com/kata/545a4c5a61aa4c6916000755/train/python
+#You have passed all of the tests! :)
