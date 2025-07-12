a3ef4903abdcc14885bdd74bbc3950123d6feafe
Travis <sneakyplutosaturn@gmail.com>
2025-07-13 09:14:27 +1000

    CodeWars 7kyu: Added solution for str_to_float.py

---
+++ 7kyu/Python/str_to_float.py
@@ -0,0 +1,9 @@
+def to_float_array(arr): 
+    # your code here
+    result_list = []
+    # Loop through the arr
+    for string in arr:
+        result_list.append(float(string))
+    return result_list
+#https://www.codewars.com/kata/5783d8f3202c0e486c001d23/train/python
+#You have passed all of the tests! :)
