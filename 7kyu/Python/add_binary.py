483d6fad20b1bf4d1d08847c562bd3aacf9b1f4c
Travis <sneakyplutosaturn@gmail.com>
2025-07-17 12:16:14 +1000

    CodeWars 7kyu: Added solution for add_binary.py

---
+++ 7kyu/Python/add_binary.py
@@ -0,0 +1,13 @@
+def add_binary(a,b):
+    #your code here
+    
+    # 1: Add inputs
+    decimal_sum = a + b
+
+    #2: Convert sum to binary
+    binary_result = bin(decimal_sum)
+
+    #3: Format the binary string
+    return binary_result[2:]
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/551f37452ff852b7bd000139/train/python
