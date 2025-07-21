c0c3f193b79a671395647d5b3ba1e49ba1b582f9
Travis <sneakyplutosaturn@gmail.com>
2025-07-21 12:01:32 +1000

    CodeWars 8kyu: Add solution for arr_doubled.py

---
+++ 8kyu/Python/arr_doubled.py
@@ -0,0 +1,10 @@
+def maps(a):
+    doubled = []
+    double = 0
+    # Append all values into doubled
+    for num in a:
+        doubled.append(num * 2)
+    return doubled
+
+#https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/python
+#You have passed all of the tests! :)
