3cd49bba5c178ed306e7ea7f8f8e902f751b2c27
Travis <sneakyplutosaturn@gmail.com>
2025-07-25 11:52:28 +1000

    CodeWars 8kyu: Complete solution CalcAverage.java

---
+++ Java/CodewarsJava/src/kyu8/CalcAverage.java
@@ -0,0 +1,14 @@
+package kyu8;
+
+public class CalcAverage {
+    public static double findAverage(int[] array) {
+        double total = 0;
+        int arrayLen = array.length;
+        for (int i = 0; i < arrayLen; i++) {
+            total += array[i];
+        }
+        return total / arrayLen;
+    }
+}
+//https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/java
+//You have passed all of the tests! :)
