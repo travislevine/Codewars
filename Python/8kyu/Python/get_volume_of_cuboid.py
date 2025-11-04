3a9481ca84d56cc3b67966ac491db9362b03bb22
Travis <sneakyplutosaturn@gmail.com>
2025-11-05 07:37:49 +1000

    CodeWars 8kyu: Complete get_volume_of_cuboid.py

---
+++ Python/8kyu/Python/get_volume_of_cuboid.py
@@ -0,0 +1,5 @@
+def get_volume_of_cuboid(length, width, height):
+    # Code goes here...
+    return length * width * height
+    #You have passed all of the tests! :)
+    #https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/python
