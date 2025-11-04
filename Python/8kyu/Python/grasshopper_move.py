48f1b96f713d2c611585636bf383ee27a6a7db24
Travis <sneakyplutosaturn@gmail.com>
2025-11-05 07:29:15 +1000

    CodeWars 8kyu: Complete solution grasshopper_move.py

---
+++ Python/8kyu/Python/grasshopper_move.py
@@ -0,0 +1,5 @@
+def move(position, roll):
+    # your code here
+    return (roll * 2) + position
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/train/python
