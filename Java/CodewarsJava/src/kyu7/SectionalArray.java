a1f1200f09971b43ee660b5af3411a5f6c45c778
Travis <sneakyplutosaturn@gmail.com>
2025-11-04 15:42:34 +1000

    CodeWars 8kyu: Complete solution opposites_attract.py

--- Java/CodewarsJava/src/kyu7/SectionalArray.java
+++ Java/CodewarsJava/src/kyu7/SectionalArray.java
@@ -1,5 +1,13 @@
 package kyu7;
 
-public class SectionalArray {
+import java.util.Arrays;
 
+public class SectionalArray {
+    static int[] sort(final int[] array, final int start, final int length = 0) {
+        return null;
+
+        if (length == 0) {
+            Arrays.sort();
+        }
+    }
 }
---
+++ Python/8kyu/Python/opposite_attract.py
@@ -0,0 +1,6 @@
+def lovefunc( flower1, flower2 ):
+    if flower1 % 2 == 0 and flower2  % 2 != 0 or flower1 % 2 != 0 and flower2  % 2 == 0 : 
+        return True
+    return False
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/555086d53eac039a2a000083/train/python
