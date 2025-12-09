9ae105133a47c0b846165aab247e6d149a578a53
Travis <sneakyplutosaturn@gmail.com>
2025-12-09 20:57:16 +1000

    CodeWars 7kyu: Start getMaxNumArrays.js

---
+++ Javascript/7kyu/getMaxNumArray.js
@@ -0,0 +1,12 @@
+// return the two oldest/oldest ages within the array of ages passed in.
+function getMaxNumArray(ages){
+    var max = 0;
+    for (let i = 0; i < ages.length - 1; i++) {
+        if (ages[i] > max) {
+            max = ages[i];
+            } 
+    }
+    return max;
+}
+
+console.log(getMaxNumArray([4, 8, 3, 12, 24, 10, 367]));
