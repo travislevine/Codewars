c846507caf27596d32be35c1e39bfa407711226c
Travis <sneakyplutosaturn@gmail.com>
2025-12-09 20:26:48 +1000

    CodeWars 8kyu: Complete evenOrOdd.js

--- Javascript/8kyu/7kyu/evenOrOdd.js
+++ Javascript/8kyu/7kyu/evenOrOdd.js
@@ -1,3 +1,7 @@
 function evenOrOdd(number) {
-  
+  if (number % 2 == 0) {
+    return "Even"
+  } return "Odd"
 }
+//You have passed all of the tests! :)
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
