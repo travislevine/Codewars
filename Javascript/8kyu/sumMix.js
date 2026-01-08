339b4e5ddc475415265e25cc68d295a24404f379
Travis <sneakyplutosaturn@gmail.com>
2026-01-08 23:01:15 +1000

    CodeWars 8kyu WIP: sumMix.js

---
+++ Javascript/8kyu/sumMix.js
@@ -0,0 +1,9 @@
+function sumMix(x){
+    let sum = 0;
+
+    for (let index = 0; index < x.length; index++) {
+        if x[index] !is Number:
+        convert to String
+        convertedNum += sum
+    }
+}
