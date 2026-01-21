a6d3abd8268c8b9ad0a8f94076fada36550826f1
Travis <sneakyplutosaturn@gmail.com>
2026-01-21 10:35:38 +1000

    CodeWars 8kyu: Complete solution personalised_greet.py

---
+++ Python/8kyu/Python/personalised_greet.py
@@ -0,0 +1,8 @@
+def greet(name, owner):
+    # Add code here
+    if name == owner:
+        return "Hello boss"
+    else:
+        return "Hello guest"
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/5772da22b89313a4d50012f7/train/python
