5124580662d762d27bdd1eb9d19f0fb7762442a0
Travis <sneakyplutosaturn@gmail.com>
2026-01-21 10:33:10 +1000

    CodeWars 8kyu: Complete solution check_for_factor.py

---
+++ Python/8kyu/Python/check_for_factor.py
@@ -0,0 +1,9 @@
+def check_for_factor(base, factor):
+    # your code here
+    if base % factor == 0:
+        return True
+    else:
+        return False
+print(check_for_factor(10, 2))
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/python
