ae45c3ace073a0ca9725330a030d15306174afdd
Travis <sneakyplutosaturn@gmail.com>
2026-01-27 08:28:06 +1000

    CodeWars 8kyu: Add solution arithmetic_calculator.py

---
+++ Python/8kyu/Python/arithmetic_calculator.py
@@ -0,0 +1,13 @@
+def arithmetic(a, b, operator):
+    #your code here
+    if operator == "add":
+        return a + b
+    elif operator == "subtract":
+        return a - b
+    elif operator == "multiply":
+        return a * b
+    elif operator == "divide":
+        return a / b
+
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/583f158ea20cfcbeb400000a/train/python
