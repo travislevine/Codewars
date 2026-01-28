e3eb701b5a5515bbd6d7ddf272f4f898d3a81eef
Travis <sneakyplutosaturn@gmail.com>
2026-01-28 18:32:14 +1000

    CodeWars 8kyu: Add solution count_sheeps.py

---
+++ Python/8kyu/Python/count_sheeps.py
@@ -0,0 +1,8 @@
+def count_sheeps(sheep):
+  count = 0
+  for animal in sheep:
+    if animal == True:
+      count += 1
+  return count
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/54edbc7200b811e956000556/train/python
