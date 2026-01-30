6bf6a32fa540750b0bf7f336ba8e99f8eb8f9966
Travis <sneakyplutosaturn@gmail.com>
2026-01-30 21:42:40 +1000

    CodeWars 8kyu: Add solution find_max_find_min.py

---
+++ Python/8kyu/Python/find_max_find_min.py
@@ -0,0 +1,10 @@
+def minimum(arr):
+    #your code here...
+    return min(arr)
+def maximum(arr):
+    #...and here
+    return max(arr)
+
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/577a98a6ae28071780000989/train/python
+#Manual implementation needed
