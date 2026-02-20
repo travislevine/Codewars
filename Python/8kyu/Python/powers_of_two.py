5cc92b1fda6e790883bce78c8218a97e688b61b4
Travis <sneakyplutosaturn@gmail.com>
2026-02-20 23:10:36 +1000

    CodeWars 8kyu: Add solution powers_of_two.py

---
+++ Python/8kyu/Python/powers_of_two.py
@@ -0,0 +1,6 @@
+def powers_of_two(n):
+    result = []
+    for i in range(n + 1):
+        result.append(2 ** i)
+    return result
+#https://www.codewars.com/kata/57a083a57cb1f31db7000028/train/python
