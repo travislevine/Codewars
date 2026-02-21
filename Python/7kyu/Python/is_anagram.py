6f9abfb64070824824042f688c7c6323b92d049b
Travis <sneakyplutosaturn@gmail.com>
2026-02-21 22:14:00 +1000

    CodeWars 8kyu: Add solution is_anagram.py

---
+++ Python/7kyu/Python/is_anagram.py
@@ -0,0 +1,5 @@
+# write the function is_anagram
+def is_anagram(test, original):
+    return sorted(test.lower()) == sorted(original.lower())
+#http://codewars.com/kata/529eef7a9194e0cbc1000255/train/python
+#You have passed all of the tests! :)
