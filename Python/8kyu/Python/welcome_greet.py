6115cf88e65a2a2ef86ccb7fca70396f48a4e6a0
Travis <sneakyplutosaturn@gmail.com>
2026-02-24 22:30:17 +1000

    CodeWars 8kyu: Add incomplete solution welcome_greet.py

---
+++ Python/8kyu/Python/welcome_greet.py
@@ -0,0 +1,22 @@
+languages_list = [
+    ("english", "Welcome"),
+    ("czech", "Vitejte"),
+    ("danish", "Velkomst"),
+    ("dutch", "Welkom"),
+    ("estonian", "Tere tulemast"),
+    ("finnish", "Tervetuloa"),
+    ("flemish", "Welgekomen"),
+    ("french", "Bienvenue"),
+    ("german", "Willkommen"),
+    ("irish", "Failte"),
+    ("italian", "Benvenuto"),
+    ("latvian", "Gaidits"),
+    ("lithuanian", "Laukiamas"),
+    ("polish", "Witamy"),
+    ("spanish", "Bienvenido"),
+    ("swedish", "Valkommen"),
+    ("welsh", "Croeso")
+]
+
+def greet(language):
+    pass # your code here
