4c5b51ba74cfd293133870ca7b6dd6fa9a247e15
Travis <sneakyplutosaturn@gmail.com>
2026-02-25 12:47:53 +1000

    CodeWars 8kyu: Update incomplete solution welcome_greet.py - solution now correct and complete

--- Python/8kyu/Python/welcome_greet.py
+++ Python/8kyu/Python/welcome_greet.py
@@ -1,22 +1,31 @@
-languages_list = [
-    ("english", "Welcome"),
-    ("czech", "Vitejte"),
-    ("danish", "Velkomst"),
-    ("dutch", "Welkom"),
-    ("estonian", "Tere tulemast"),
-    ("finnish", "Tervetuloa"),
-    ("flemish", "Welgekomen"),
-    ("french", "Bienvenue"),
-    ("german", "Willkommen"),
-    ("irish", "Failte"),
-    ("italian", "Benvenuto"),
-    ("latvian", "Gaidits"),
-    ("lithuanian", "Laukiamas"),
-    ("polish", "Witamy"),
-    ("spanish", "Bienvenido"),
-    ("swedish", "Valkommen"),
-    ("welsh", "Croeso")
-]
+languages_dict = {
+    "english": "Welcome",
+    "czech": "Vitejte",
+    "danish": "Velkomst",
+    "dutch": "Welkom",
+    "estonian": "Tere tulemast",
+    "finnish": "Tervetuloa",
+    "flemish": "Welgekomen",
+    "french": "Bienvenue",
+    "german": "Willkommen",
+    "irish": "Failte",
+    "italian": "Benvenuto",
+    "latvian": "Gaidits",
+    "lithuanian": "Laukiamas",
+    "polish": "Witamy",
+    "spanish": "Bienvenido",
+    "swedish": "Valkommen",
+    "welsh": "Croeso"
+}
 
 def greet(language):
-    pass # your code here
+    if not isinstance(language, str):
+        return languages_dict["english"]
+    normalised = language.lower()
+    if normalised in languages_dict:
+        return languages_dict[normalised]
+    else:
+        return languages_dict["english"]
+print(greet("fleMish"))
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/577ff15ad648a14b780000e7/train/python
