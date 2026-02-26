886ea96b38df4f4d61f3f48472440cb410394c50
Travis <sneakyplutosaturn@gmail.com>
2026-02-26 18:36:38 +1000

    CodeWars 8kyu: Add ping_pong.py, solution in progress.

---
+++ Python/6kyu/python/ping_pong.py
@@ -0,0 +1,8 @@
+def ping_pong(sounds):
+    # Your code here!
+    working_list = sounds.split()
+    score_mr_ping = 0
+    score_mrs_ping = 0
+    current_server = ...
+    last_hitter = ...
+    return "ping"  # or "pong"
