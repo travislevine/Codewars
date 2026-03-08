360a7dd274e02666e52435f0cdbe0ec4f9719356
Travis <sneakyplutosaturn@gmail.com>
2026-03-08 22:09:17 +1000

    CodeWars 8kyu: Add structure for get_average.py

--- Python/6kyu/python/ping_pong.py
+++ Python/6kyu/python/ping_pong.py
@@ -1,8 +1,15 @@
 def ping_pong(sounds):
     # Your code here!
-    working_list = sounds.split()
+    working_list = sounds.split('-')
     score_mr_ping = 0
-    score_mrs_ping = 0
-    current_server = ...
-    last_hitter = ...
+    score_mr_pong = 0
+    current_server = None
+    last_hitter = None
+    last_rally_winner = None
+
+    for hit in working_list:
+        if not hit == "ping" or not hit == "pong":
+            continue
+        
+
     return "ping"  # or "pong"
---
+++ Python/8kyu/Python/get_average.py
@@ -0,0 +1,2 @@
+def get_average(marks):
+    raise NotImplementedError("TODO: get_average")
