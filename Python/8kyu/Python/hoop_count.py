9786ab26ee8782c18d1ee9fc4803c8a25e2451b6
Travis <sneakyplutosaturn@gmail.com>
2026-03-11 21:43:41 +1000

    CodeWars 8kyu: Add solution for hoop_count.py

---
+++ Python/8kyu/Python/hoop_count.py
@@ -0,0 +1,8 @@
+def hoop_count(n):
+    # Good Luck!
+    if n >= 10:
+        return "Great, now move on to tricks"
+    else:
+        return "Keep at it until you get it"
+    #You have passed all of the tests! :)
+    #https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/python
