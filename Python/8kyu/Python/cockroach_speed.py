a981e47444f3303cde55541a6c9a039a75404bd0
Travis <sneakyplutosaturn@gmail.com>
2026-03-11 21:50:10 +1000

    CodeWars 8kyu: Fix solution for cockroach_speed.py - change round function to floor technique instead

--- Python/8kyu/Python/cockroach_speed.py
+++ Python/8kyu/Python/cockroach_speed.py
@@ -1,6 +1,6 @@
 def cockroach_speed(s):
     # Good Luck!
     speed = s * 27.778
-    return round(speed)
+    return speed // 1
 #You have passed all of the tests! :)
 #https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/python
