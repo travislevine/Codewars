3f824fb5a6e18269519aa23fb81ba68e583e4648
Travis <sneakyplutosaturn@gmail.com>
2026-03-18 21:25:28 +1000

    CodeWars 8kyu: Add solution for between_integer.py

---
+++ Python/8kyu/Python/between_integer.py
@@ -0,0 +1,9 @@
+def between(a,b):
+    # good luck
+    empty = []
+    for i in range(a, b + 1):
+        empty.append(i)
+    return empty
+
+#You have passed all of the tests! :)
+#https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/python
