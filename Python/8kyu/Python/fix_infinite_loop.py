1f4755ee81ed1f958df794f27e465d4392f7d187
Travis <sneakyplutosaturn@gmail.com>
2026-03-19 20:52:44 +1000

    CodeWars 8kyu: Add solution for fix_infinite_loop.py

---
+++ Python/8kyu/Python/fix_infinite_loop.py
@@ -0,0 +1,18 @@
+def create_array(n):
+    res=[]
+    i=1
+    while i<=n: 
+        res+= [i]
+        i+= 1
+    return res
+
+#https://www.codewars.com/kata/55c28f7304e3eaebef0000da/train/python
+#You have passed all of the tests! :)
+
+#original loop
+# def create_array(n):
+#     res=[]
+#     i=1
+#     while i<=n: res+=[i]
+#     return res
+
